f3d5137aa65d3e9127b2519ce2fb996cfd14b0b4
Puthi-sgr <chethaputhi@gmail.com>
2025-04-20 15:40:48 +0700

    build auth controller

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+
+class AuthController{
+    private User $userModel;
+
+    public function __construct()
+    {   
+        $this->userModel = new User();
+        session_start();
+    }
+
+    public function register(){
+        $email = $_POST['email'] ?? '';
+        $password = $_POST['password'] ?? '';
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL) || strlen($password) < 6){
+            http_response_code(422);
+            echo "Invalid email or password.";
+            return;
+        }
+
+        $isEmailExisted = $this->userModel->findByEmail($email);
+        if($isEmailExisted){
+            http_response_code(409);
+            echo "Email is already registered";
+            return;
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $result = $this->userModel->create($email, $hashedPassword);
+
+        if($result){
+            echo "Account has been successfully created";
+        }else{
+            http_response_code(404);
+            echo "Registration failed";
+        }
+    }
+}
